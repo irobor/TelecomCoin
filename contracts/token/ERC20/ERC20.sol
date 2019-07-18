0643712db64f1af8bd1917cffe0cbc110b7ffea8
irobor <lion.vvm@gmail.com>
2019-07-18 12:26:15 +0300

    Update and rename IERC20.sol to ERC20.sol

---
+++ contracts/token/ERC20/ERC20.sol
@@ -0,0 +1,158 @@
+pragma solidity ^0.5.0;
+
+import "browser/SafeMath.sol" ;
+import "browser/IERC20.sol" ;
+
+/**
+ * @dev Implementation of the `IERC20` interface.
+ *
+ * 
+ */
+ 
+ contract ERC20 is IERC20 {
+	 
+	using SafeMath for uint256;
+	
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+	
+    uint256 private _totalSupply;
+	 
+    /**
+     * @dev See `IERC20.totalSupply`.
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See `IERC20.balanceOf`.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See `IERC20.transfer`.
+     */
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See `IERC20.allowance`.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev See `IERC20.approve`.
+     */
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+ 
+ 
+	 /**
+     * @dev See `IERC20.transferFrom`.
+     *
+     * Emits an `Approval` event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of `ERC20`;
+     *
+	 * Я не проведу amount от sender к recipient пока не будет _allowances = amount
+     * Requirements:
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `value`.
+     * - the caller must have allowance for `sender`'s tokens of at least
+     * `amount`.
+     */
+   
+	 function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
+        return true;
+    }
+    
+	/** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a `Transfer` event with `from` set to the zero address.
+     */
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+	
+	/**
+     * @dev Destoys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a `Transfer` event with `to` set to the zero address.
+     */
+
+	function _burn(address account, uint256 amount) internal {
+		require(account != address(0), "ERC20: burn from the zero address");
+		require(amount <= _balances[account]);
+		// no need to require value <= totalSupply, since that would imply the
+		// sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+		_balances[account] = _balances[account].sub(amount);
+		_totalSupply = _totalSupply.sub(amount);		
+		emit Transfer(account, address(0), amount);
+	}
+	
+	/**
+     * @dev Moves tokens `amount` from `sender` to `recipient`.
+     *
+     * This is internal function is equivalent to `transfer`, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a `Transfer` event.
+     */
+	function _transfer(address sender, address recipient, uint256 amount) internal{	
+		require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");	
+		require(amount <= _balances[sender]);
+				
+
+		_balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+		
+        emit Transfer(sender, recipient, amount);
+		
+    }
+	
+	/**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
+     *
+     * Emits an `Approval` event.
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+		require(value <= _balances[owner]);
+
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+	/**
+     * @dev Destoys `amount` tokens from `account`.`amount` is then deducted
+     * from the caller's allowance.
+     *
+     * See `_burn` and `_approve`.
+     */
+    function _burnFrom(address account, uint256 amount) internal {
+        _burn(account, amount);
+        _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount));
+    }
+ 
+ }
