41ecc144b63f8d868fc9bba36e575a9b70fdb670
irobor <lion.vvm@gmail.com>
2019-07-20 11:25:26 +0300

    Update CrowdsaleERC20.sol

--- contracts/crowdsale/CrowdsaleERC20.sol
+++ contracts/crowdsale/CrowdsaleERC20.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
 
-import "./token/ERC20/TelecomCoin.sol";
-import "./utils/ReentrancyGuard.sol" ;
+import "../token/ERC20/TelecomCoin.sol";
+import "../utils/ReentrancyGuard.sol" ;
 
 
 contract Crowdsale is ReentrancyGuard{
